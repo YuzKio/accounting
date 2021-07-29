dcaf0bc2e4e6afaa6590c8d2e2d7f67a1156c262
unknown <407249117@qq.com>
2021-07-29 22:24:48 +0800

    new file

---
+++ src/views/Summary/components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>test</div>
+</template>
+
+<script>
+export default {
+  name: 'SummaryCard'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Summary/index.vue
+++ src/views/Summary/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <summary-header></summary-header>
+    <summary-card></summary-card>
   </div>
 </template>
 
 <script>
 import SummaryHeader from './components/Header'
+import SummaryCard from './components/Card'
 export default {
   name: 'Summary',
-  components: {SummaryHeader}
+  components: {SummaryHeader, SummaryCard}
 }
 </script>
